521ea6285b9bce0764acdfd78ef14e71e80c4250
DavGuett <soulsec@protonmail.com>
2022-11-07 20:10:10 -0300

    Inicio do modo de edição

---
+++ EditorHTML/Editor.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+
+namespace EditorHTML;
+
+public static class Editor
+{
+    public static void Show()
+    {
+        Console.Clear();
+        Console.BackgroundColor = ConsoleColor.White;
+        Console.ForegroundColor = ConsoleColor.Black;
+        Console.Clear();
+        Console.WriteLine("Modo de edição");
+        Start();
+    }
+
+    public static void Start()
+    {
+        var file = new StringBuilder();
+
+        do
+        {
+            file.Append(Console.ReadLine());
+            file.Append(Environment.NewLine);
+        } while (Console.ReadKey().Key != ConsoleKey.Escape);
+    }
+}
